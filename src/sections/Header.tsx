791961137560affa493932d5766caf6a7ba37c82
Emmanuel Oppong <107645887+emmaeng680@users.noreply.github.com>
2024-12-03 13:03:44 -0600

    circles
    circ

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,6 +1,8 @@
-export const Header = () => {
+
+
+ export const Header = () => {
   return (
-      <div className='flex justify-center items-center relative top-3'>
+      <div className='flex justify-center items-center fixed top-3 z-10 w-full'>
           <nav className='flex gap-1 p-0.5 border border-white/15 rounded-full bg-white/10 backdrop-blur'>
               <a href='#' className='nav-item'>Home</a>
               <a href='#' className='nav-item'>Projects</a>
