512af9dccccd94c96f0efa8f1282bfe3cf2772a9
Emmanuel Oppong <107645887+emmaeng680@users.noreply.github.com>
2025-01-05 22:47:01 -0600

    updates
    updated

--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -1,3 +1,43 @@
+
+import ArrowUpRightIcon from "@/assets/icons/arrow-up-right.svg"
+
+
+const footerLinks = [
+  {
+    title: 'LinkedIn',
+    href: '#',
+  },
+  {
+    title: 'Twitter',
+    href: '#',
+  },
+  {
+    title: 'Instagram',
+    href: '#',
+  },
+  {
+    title: 'Leetcode',
+    href: '#',
+  },
+]
+
 export const Footer = () => {
-  return <div>Footer Section</div>;
+  return (
+      <footer>
+        <div className="container">
+          <div>
+            <div>&copy; 2025. All rights reserved.</div>
+            <nav>
+              {footerLinks.map(link => (
+                  <a href="" key={link.title}>
+                    <span>{link.title}</span>
+                    <ArrowUpRightIcon />
+                  </a>
+
+              ))}
+            </nav>
+          </div>
+        </div>
+      </footer>
+  )
 };
